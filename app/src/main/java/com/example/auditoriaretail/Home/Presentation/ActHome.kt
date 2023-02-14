622923b87a39191019af8b0752f0c89ba8aae935
JonaRambo <jonaulig@gmail.com>
2023-02-13 19:37:25 -0600

    "Creacion de Splah_Screen"

---
+++ app/src/main/java/com/example/auditoriaretail/Home/Presentation/ActHome.kt
@@ -0,0 +1,12 @@
+package com.example.auditoriaretail.Home.Presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.auditoriaretail.R
+
+class ActHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_home)
+    }
+}
--- app/src/main/java/com/example/auditoriaretail/MainActivity.kt
+++ app/src/main/java/com/example/auditoriaretail/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.example.auditoriaretail
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.auditoriaretail.Home.Presentation.ActHome
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//Codigo para quitar la barra, duracion de la pantalla y el intento del cambio de pantalla
+        supportActionBar?.hide()
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(6000L)
+            startActivity(Intent(this@MainActivity, ActHome::class.java))
+            finish()
+        }
     }
 }
 
 /* ESTOS SON MIS CAMBIOS*/
 
-// JONATHAN SUBE TUS CAMBIOS CON TU RAMA
