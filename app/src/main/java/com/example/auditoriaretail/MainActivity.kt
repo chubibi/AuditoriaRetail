17a23e56cc4692f029760d6d7410d6ab1a709277
chubibi <josue.cazares.10@gmail.com>
2023-02-13 18:31:10 -0600

    "Proyecto Inicial a rama Principal"

---
+++ app/src/main/java/com/example/auditoriaretail/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.auditoriaretail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+/* ESTOS SON MIS CAMBIOS*/
+
+// JONATHAN SUBE TUS CAMBIOS CON TU RAMA
