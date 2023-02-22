eec6e401746db6793fab782c2ac527d9168992c4
chubibi <josue.cazares.10@gmail.com>
2023-02-21 22:18:40 -0600

    Feature - Add Injection Dependency and Firebase

---
+++ app/src/main/java/com/example/auditoriaretail/Dashboard/data/model/User.kt
@@ -0,0 +1,8 @@
+package com.example.auditoriaretail.Dashboard.data.model
+
+data class User(
+    var id:Int,
+    var nombre:String,
+    var email:String,
+    var password:String
+)
--- app/src/main/java/com/example/auditoriaretail/Login/presentation/ui/FrgMainLogin.kt
+++ app/src/main/java/com/example/auditoriaretail/Login/presentation/ui/FrgMainLogin.kt
@@ -1,13 +1,16 @@
 package com.example.auditoriaretail.Login.presentation.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.auditoriaretail.R
 import com.example.auditoriaretail.databinding.FrgMainLoginBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class FrgMainLogin : Fragment() {
@@ -25,6 +28,21 @@ class FrgMainLogin : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val user:MutableMap<String,Any> = HashMap()
+        user["password"] = "1234"
+        user["email"] = "josue.cazares.10@gmail.com"
+        //Testing firestore
+        FirebaseFirestore.getInstance().collection("users")
+            .add(user)
+            .addOnSuccessListener {
+                Toast.makeText(requireContext(), "->${it.id.toString()}", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {
+                Log.d("FIREB","Error en firebase")
+            }
+
+
+
 
         binding.btnRegistrar.setOnClickListener {
             findNavController().navigate(R.id.to_frg_register)
